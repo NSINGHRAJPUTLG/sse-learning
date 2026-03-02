e8b0daaa28d55b0ea5773d05dd103d23fd2991d9
lz-28 <lz-28@lz-28s-Mac-mini.local>
2026-03-02 17:30:07 +0530

    Track sse as regular directory instead of submodule

---
+++ sse/app/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const eventSource = new EventSource("http://localhost:4000/stream");
+
+    eventSource.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      setCount(data.count);
+    };
+
+    eventSource.onerror = (err) => {
+      console.error("SSE error:", err);
+      eventSource.close();
+    };
+    console.log('setting stream data')
+    return () => {
+      eventSource.close();
+    };
+  }, []);
+
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Server Counter:</h1>
+      <h2>{count}</h2>
+    </div>
+  );
+}
