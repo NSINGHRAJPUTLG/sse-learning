5c4045d0537f262938f53d929ae9e243737c32e7
lz-28 <lz-28@lz-28s-Mac-mini.local>
2026-03-05 10:44:43 +0530

    deployment configurations4

--- sse/lib/api.ts
+++ sse/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useAuthStore } from '@/store/auth.store';
 
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000/api';
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://api.nsrgfx.in/api';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
